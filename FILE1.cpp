72c4a9a6d2f3572130ed274991e606328ffe00d4
Muhammad Saqib <242281@students.au.edu.pk>
2024-12-16 12:01:16 +0500

    printed value of a

--- FILE1.cpp
+++ FILE1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+	int a;
 	cout << "Hello word!";
-
+	cout << "Enter the value of a";
+	cin >> a;
+	cout << "Value of a : " << a;
 	return 0;
 }
