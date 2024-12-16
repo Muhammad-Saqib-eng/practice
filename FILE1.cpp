f090003ca6adf3979ae2791f9f1258f8e8ae5327
Muhammad Saqib <242281@students.au.edu.pk>
2024-12-16 12:07:47 +0500

    introduced an array

--- FILE1.cpp
+++ FILE1.cpp
@@ -6,5 +6,12 @@ int main() {
 	cout << "Enter the value of a";
 	cin >> a;
 	cout << "Value of a : " << a;
+
+	cout << "\nIntroduce an array";
+	int arr[5] = { 1, 2, 3, 4, 5 };
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i];
+	}
+	
 	return 0;
 }
